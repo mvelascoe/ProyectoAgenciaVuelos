9683291f927bf6aa24c3ce8df6b7e10b82dc6193
saraLozano <151969212+SaraLuciaLozanoRueda@users.noreply.github.com>
2024-06-24 09:06:16 -0500

    IN airline

--- src/main/java/com/proyectojava/airline/adapters/in/AirlineConsoleAdapter.java
+++ src/main/java/com/proyectojava/airline/adapters/in/AirlineConsoleAdapter.java
@@ -1,5 +1,48 @@
 package com.proyectojava.airline.adapters.in;
 
-public class AirlineConsoleAdapter {
+import java.util.Scanner;
 
+import com.proyectojava.airline.application.AirlineService;
+
+public class AirlineConsoleAdapter {
+    private final AirlineService airlineService;
+
+    public AirlineConsoleAdapter(AirlineService airlineService) {
+        this.airlineService = airlineService;
+    }
+
+    public void startAirline(){
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            System.out.println("1. Crear aerolinea\2. \n2. Actualizar informacion de aerolinea\n3. Buscar aerolinea\n4. Eliminar aerolinea\n5. Listar todas las aerolineas\n6. Volver\n\n");
+            System.out.println("Ingresa una opcion");
+            int opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+                System.out.println("Volviendo al menu principal... Adios");
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+    }
 }
