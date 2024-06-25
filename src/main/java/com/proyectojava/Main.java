8c0d941aced6534b90d2b569b8a761cf810b2f96
mvelascoe <166876606+mvelascoe@users.noreply.github.com>
2024-06-25 06:55:52 -0500

    Feat: prueba

--- src/main/java/com/proyectojava/Main.java
+++ src/main/java/com/proyectojava/Main.java
@@ -3,7 +3,7 @@ package com.proyectojava;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Agencia Vuelos Globales1");
+        System.out.println("Agencia Vuelos Globales2");
 
 
     }
